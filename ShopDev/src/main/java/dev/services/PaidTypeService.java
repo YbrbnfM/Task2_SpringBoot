532e2ff43a20fb9b33a4043882e5fa34983c21a9
YbrbnfM <nsm.mainmail@gmail.com>
2019-12-11 23:38:38 +0400

    [CustomerNode]Добавление: сервис операций над типами платежей (PaidType)

---
+++ ShopDev/src/main/java/dev/services/PaidTypeService.java
@@ -0,0 +1,71 @@
+package dev.services;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+import dev.entities.PaidType;
+import lombok.NonNull;
+
+@Repository
+@Transactional
+public class PaidTypeService implements Service<PaidType> {
+	@PersistenceContext
+	private EntityManager em;
+
+	@SuppressWarnings("unchecked")
+	public List<PaidType> getAll() throws PersistenceException {
+		return em.createQuery("from PaidType").getResultList();
+	}
+
+	@Override
+	public List<PaidType> get(@NonNull Predicate<PaidType> p) throws PersistenceException {
+		return getAll().stream().filter(p).collect(Collectors.toList());
+	}
+
+	@Override
+	public PaidType get(int id) throws PersistenceException {
+		@SuppressWarnings("unchecked")
+		List<PaidType> paidTypes = em.createQuery("from PaidType pt where pt.id = " + id).getResultList();
+		if (paidTypes.isEmpty())
+			throw new NoSuchElementException("Остутствует элемент по заданному id");
+		return paidTypes.get(0);
+	}
+
+	/*
+	 * Определение операции по id Если id существует - обновить Если id имеет
+	 * дефолтное значение - создать
+	 */
+	@Override
+	public PaidType create_edit(@NonNull PaidType o) {
+		//TODO: Проверить на добавление с одинаковыми email и номерами телефона. Обработать потенциальные исключения
+		if (o.getId() == 0) {
+			em.persist(o);
+			return o;
+		}
+		PaidType orig = em.find(PaidType.class, o.getId());
+		orig.setName(o.getName());
+		em.merge(orig);
+		return orig;
+		/*
+		 * TODO: проверить допустим ли такой вариант
+		 * em.merge(o);
+		 */
+	}
+
+	@Override
+	public boolean delete(int id) {
+		PaidType orig = em.find(PaidType.class, id);
+		if (orig != null) {
+			em.remove(orig);
+			return true;
+		}
+		return false;
+	}
+}
